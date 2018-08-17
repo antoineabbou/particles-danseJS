85925982a0d58f7696bc714e3cd40e3e36c909c9
antoineabbou <abbouantoine@gmail.com>
2018-08-17 18:03:20 +0200

    rm node-modules and quick fix for deploy

--- node_modules/three/src/renderers/shaders/ShaderChunk/worldpos_vertex.glsl
+++
@@ -1,5 +0,0 @@
-#if defined( USE_ENVMAP ) || defined( PHONG ) || defined( PHYSICAL ) || defined( LAMBERT ) || defined( DISTANCE ) || defined ( USE_SHADOWMAP )
-
-	vec4 worldPosition = modelMatrix * vec4( transformed, 1.0 );
-
-#endif
